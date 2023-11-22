91663730f5cc9890e6ba6436d6b36d76a7ae66ac
t1mt4m <tam.k@oundleschool.org.uk>
2023-11-22 11:35:39 +0000

    basically copypaste from before

--- addusers.php
+++ addusers.php
@@ -1,1 +1,43 @@
+<?php
+array_map("htmlspecialchars", $_POST);
 
+echo("submitted");
+        echo $_POST["gender"]."<br>";
+        echo $_POST["forename"]."<br>";
+        echo $_POST["surname"]."<br>";
+        echo $_POST["house"]."<br>";
+        echo $_POST["year"]."<br>";
+        echo $_POST["passwd"]."<br>";
+        echo $_POST["role"]."<br>";
+        
+try{		
+	include_once("connection.php");
+	switch($_POST["role"]){
+		case "Pupil":
+			$role=0;
+			break;
+		case "Teacher":
+			$role=1;
+			break;
+		case "Admin":
+			$role=2;
+			break;
+	}
+	$stmt = $conn->prepare("INSERT INTO TblUsers (UserID,Gender,Surname,Forename,Password,House,Year ,Role)VALUES (null,:gender,:surname,:forename,:password,:house,:year,:role)");
+	$stmt->bindParam(':forename', $_POST["forename"]);
+	$stmt->bindParam(':surname', $_POST["surname"]);
+	$stmt->bindParam(':house', $_POST["house"]);
+	$stmt->bindParam(':year', $_POST["year"]);
+	$stmt->bindParam(':password', $_POST["passwd"]);
+	$stmt->bindParam(':gender', $_POST["gender"]);
+	$stmt->bindParam(':role', $role);
+	$stmt->execute();
+	}
+catch(PDOException $e)
+	{
+		echo "error".$e->getMessage();
+	}
+	
+$conn=null;
+header('Location: users.php');
+?>
