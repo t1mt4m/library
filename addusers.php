f47253dff74919cc2fecbdb7fb2b2692f3b7e8bc
t1mt4m <tam.k@oundleschool.org.uk>
2023-11-24 10:07:33 +0000

    adapted some changes to fit library system

--- addusers.php
+++ addusers.php
@@ -2,13 +2,9 @@
 array_map("htmlspecialchars", $_POST);
 
 echo("submitted");
-        echo $_POST["gender"]."<br>";
         echo $_POST["forename"]."<br>";
         echo $_POST["surname"]."<br>";
-        echo $_POST["house"]."<br>";
-        echo $_POST["year"]."<br>";
         echo $_POST["passwd"]."<br>";
-        echo $_POST["role"]."<br>";
         
 try{		
 	include_once("connection.php");
@@ -23,13 +19,10 @@ try{
 			$role=2;
 			break;
 	}
-	$stmt = $conn->prepare("INSERT INTO TblUsers (UserID,Gender,Surname,Forename,Password,House,Year ,Role)VALUES (null,:gender,:surname,:forename,:password,:house,:year,:role)");
+	$stmt = $conn->prepare("INSERT INTO TblUsers (UserID,Surname,Forename,Password,Role)VALUES (null,:gender,:surname,:forename,:password,:role)");
 	$stmt->bindParam(':forename', $_POST["forename"]);
 	$stmt->bindParam(':surname', $_POST["surname"]);
-	$stmt->bindParam(':house', $_POST["house"]);
-	$stmt->bindParam(':year', $_POST["year"]);
 	$stmt->bindParam(':password', $_POST["passwd"]);
-	$stmt->bindParam(':gender', $_POST["gender"]);
 	$stmt->bindParam(':role', $role);
 	$stmt->execute();
 	}
--- users.php
+++ users.php
@@ -10,18 +10,12 @@
 		First name:<input type="text" name="forename"><br>
 		Last name:<input type="text" name="surname"><br>
 		Password:<input type="password" name="passwd"><br>
-		House:<input type="text" name="house"><br>
-		Year:<input type="text" name="year"><br>
 		<!--Creates a drop down list-->
-		Gender:<select name="gender">
-			  <option value="M">Male</option>
-			  <option value="F">Female</option>
-		  </select>
 		<br>
 		<!--Next 3 lines create a radio button which we can use to select the user role-->
-		<input type="radio" name="role" value="Pupil" checked> Pupil<br>
-		<input type="radio" name="role" value="Teacher"> Teacher<br>
-		<input type="radio" name="role" value="Admin"> Admin<br>
+		<input type="radio" name="role" value="user" checked> user<br>
+		<input type="radio" name="role" value="librarian"> librarian<br>
+		<input type="radio" name="role" value="admin"> admin<br>
 		<input type="submit" value="Add User">
 	  </form> 
       
